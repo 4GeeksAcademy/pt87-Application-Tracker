c95463ce9c013ab450a2f9f4558b647d3068681a
TJ Viaud <tjviaud@gmail.com>
2026-02-12 23:37:01 +0000

    added navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,51 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        
+        <Link to="/">
+                    <span className="navbar-brand mb-0 h1">ApplyTrack</span>
+                </Link>
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+        {/* Toggle Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible Content */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/applications">Applications</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/add-job">Add Job</Link>
+            </li>
+          </ul>
+
+          {/* Right Side */}
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/logout">
+                Logout <i className="fa-solid fa-arrow-right-from-bracket"></i>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
